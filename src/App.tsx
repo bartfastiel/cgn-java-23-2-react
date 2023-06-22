ee86969dd0106c5560adaeb8ce8ebd6fcb74a986
Daniel Schwarz <bartfastiel@users.noreply.github.com>
2023-06-22 09:24:21 +0200

    Add birthday example

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,18 @@
-import Title from "./Title.tsx";
-import Message from "./Message.tsx";
-import BlogPost from "./BlogPost.tsx";
+import {useState} from "react";
 
 export default function App() {
 
-    const myPersonalNotes = [
-        {
-            text: "Heute ist ein schöner Tag"
-        },
-        {
-            text: "Leider heute Regen"
-        },
-        {
-            text: "Leider heute so mittel"
-        },
-        {
-            text: "Leider heute Gewitter"
-        },
-    ]
+    const [age, setAge] = useState<number>(17);
+
+    function birthday() {
+        setAge(18)
+        console.log(age)
+    }
 
     return (
         <>
-            <Title></Title>
-            <Message age={38}></Message>
-            {
-                myPersonalNotes.map(entry => {
-                    return <BlogPost content={entry} key={entry.text}></BlogPost>
-                })
-            }
+            <h1>{age}</h1>
+            <button onClick={birthday}>Birthday</button>
         </>
     )
 }
